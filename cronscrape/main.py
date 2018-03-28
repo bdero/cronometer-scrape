78fa7186e2b051bcbb469e574d4d75ffee750170
Brandon DeRosier <x@bdero.me>
2018-03-28 02:49:41 -0700

    Add task route for fetching latest reports

--- cronscrape/main.py
+++ cronscrape/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from google.appengine.api.taskqueue import taskqueue
 
 from cronscrape.scrape import collect_latest_reports
 
@@ -6,10 +7,21 @@ from cronscrape.scrape import collect_latest_reports
 app = Flask(__name__)
 
 
+@app.route('/task_latest')
+def task_latest():
+    amount = request.args.get('amount', default=1, type=int)
+    return jsonify(collect_latest_reports(amount))
+
+
 @app.route('/latest')
 def latest():
     amount = request.args.get('amount', default=1, type=int)
-    return jsonify(collect_latest_reports(amount))
+    task = taskqueue.add(
+        url='/task_latest',
+        target='worker',
+        params={'amount': amount},
+    )
+    return jsonify({'task_name': task.name})
 
 
 @app.route('/_ah/health')
